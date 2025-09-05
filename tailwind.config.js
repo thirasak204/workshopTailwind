f9af147ca55c3dc7f420195e1ef5271b7bd8ff71
Thirasak <timza781@gmail.com>
2025-09-06 00:58:17 +0700

    ปรับขนาดหน้าจอ

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ export default {
         center: true,
         padding: '1rem',
         screens: {
+          'xs': '480px',
           'sm': '640px',
           'md': '768px',
           'lg': '1024px',
