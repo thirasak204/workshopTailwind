553a8a686702d492609c777e99c0e60015e92aa1
Thirasak <timza781@gmail.com>
2025-09-05 21:44:54 +0700

    tailwind project

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}"],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: '1rem',
+        screens: {
+          'sm': '640px',
+          'md': '768px',
+          'lg': '1024px',
+          'xl': '1280px',
+          '2xl': '1280px',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
+
